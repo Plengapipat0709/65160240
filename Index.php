6b5062dd451edc45b1c424f20bfef579d2e11852
Plengapipat0709 <152172282+Plengapipat0709@users.noreply.github.com>
2023-12-25 17:36:44 +0700

    example

--- Index.php
+++ Index.php
@@ -1,7 +1,20 @@
+<!DOCTYPE html>
 <html>
-
+<head>
+<meta charset="UTF-8">
+</head>
 <body>
-    <h1>This is PHP 5555fbplfkofkf55</h1>
-</body>
 
+    <?php
+        isset( $_POST['base'] ) ? $base = $_POST['base'] : $base = "";
+        if( !empty( $base ) ) {
+            echo "<hr/>";
+            echo "<b>สูตรคูณแม่ {$base}</b><br/><br/>";
+            
+        }
+        for( $i=1; $i<=24; $i++ ) {
+                echo "2 x {$i} = ".(2*$i)."<br/>";
+            }
+    ?>
+</body>
 </html>

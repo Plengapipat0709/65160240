58604b87e81603497fec5b868ab0e8d580971913
Plengapipat0709 <152172282+Plengapipat0709@users.noreply.github.com>
2023-12-29 04:20:54 +0700

    Update myroute.blade.php

--- example-app/resources/views/myroute.blade.php
+++ example-app/resources/views/myroute.blade.php
@@ -11,17 +11,17 @@
         <br><br><br>
         <B>
             <h1>ตารางสูตรคูณแม่ <?php echo $myinput; ?></h1>
-        <div class="table">
-            <?php
-            if (!empty($myinput)) {
-                for ($i = 1; $i <= 24; $i++) {
-                    echo '<p>';
-                    echo "&nbsp;$myinput x {$i} = " . $myinput * $i . '<br/>';
-                    echo '</p>';
+            <div class="table">
+                <?php
+                if (!empty($myinput)) {
+                    for ($i = 1; $i <= 24; $i++) {
+                        echo '<p>';
+                        echo "&nbsp;$myinput x {$i} = " . $myinput * $i . '<br/>';
+                        echo '</p>';
+                    }
                 }
-            }
-            ?>
-        </div>
+                ?>
+            </div>
         </B>
         <br>
         <form method="post" action="/my-page">
@@ -45,12 +45,14 @@
         font-family: Verdana;
         background-image: url(https://i.pinimg.com/originals/4e/df/0c/4edf0c16b629c17da083a80016cebc04.gif);
     }
-    body{
+
+    body {
         background-image: url(https://i.pinimg.com/originals/38/d3/0e/38d30ecfe41c21551c043443d330d637.gif);
         background-repeat: no-repeat;
         background-size: cover;
     }
-    h1{
+
+    h1 {
         color: whitesmoke;
         text-shadow: 5px 5px 5px blue;
     }

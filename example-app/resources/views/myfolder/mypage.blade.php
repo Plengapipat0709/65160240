23af43f85e3af22a41ed2ae809c52e16220de693
Plengapipat0709 <152172282+Plengapipat0709@users.noreply.github.com>
2023-12-29 03:48:17 +0700

    Update mypage.blade.php

--- example-app/resources/views/myfolder/mypage.blade.php
+++ example-app/resources/views/myfolder/mypage.blade.php
@@ -1,10 +1,38 @@
-<h1>My Folder and My Page</h1>
-<h1>
-    <?php echo $val_a . '' . $val_b; ?>
-</h1>
-<h1>{{$val_a}} {{$val_b}}</h1>
-<form method="post" action="/my-route">
-    @csrf
-    <input type="text" name="myinput">
-    <button type="submit">Submit</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Multiplication</title>
+</head>
+
+<body>
+    <center>
+
+        <form method="post" action="/my-route">
+            <h1>Multiplication Calculator</h1>
+            @csrf
+            <input type="text" name="myinput">
+            <br><br>
+            <button type="submit">Submit</button>
+        </form>
+
+    </center>
+</body>
+<style>
+    body {
+        background-image: url(https://i.pinimg.com/originals/f0/4a/0f/f04a0f964e22f64a08e892c9496f1864.gif);
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        background-attachment: fixed;
+    }
+
+    h1 {
+        margin-top: 15%;
+        color: whitesmoke;
+        font-size: 50px;
+    }
+</style>
+
+</html>
